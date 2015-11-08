ae237b5ddaa20fc6b61030adf8c397b7225fd365
Gayathri Murali <gayathri.m.softie@gmail.com>
2015-11-08 14:57:18 -0800

    Adding UDFs,SerDe,Join and JARS

--- Sql_testcases.py
+++ Sql_testcases.py
@@ -59,6 +59,57 @@ hivecontext.sql("INSERT INTO TABLE parquetTests SELECT * FROM avroTests")
 results=hivecontext.sql("SELECT * FROM parquetTests")
 results.show()
 
+#Test 8 : Add custom SerDe
+
+#hivecontext.sql("ADD JAR '/Users/gayathrimurali/.m2/repository/org/apache/hadoop/hive/serde2/TestSerDe/1.0/TestSerDe-1.0.jar'")
+hivecontext.sql("CREATE TABLE IF NOT EXISTS INPUT16_CC(KEY STRING,VALUE STRING) ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.TestSerDe'  with serdeproperties ('testserde.default.serialization.format'='\003', 'dummy.prop.not.used'='dummyy.val') STORED AS TEXTFILE")
+hivecontext.sql("LOAD DATA LOCAL INPATH 'TestSerde/kv1_cc.txt' INTO TABLE INPUT16_CC")
+results=hivecontext.sql("SELECT * FROM INPUT16_CC")
+results.show()
+
+#Adding hive UDFs
+hivecontext.sql("CREATE TEMPORARY FUNCTION TOUPPER AS 'com.example.hive.udf.ToUpper'")
+results=hivecontext.sql("SELECT TOUPPER(name) FROM users")
+results.show()
+
+
+#Performing different table joins
+#Create and insert values into 2 tables
+filepath_1=examplefiles_path + "states.txt"
+filepath_2=examplefiles_path + "gdp.txt"
+load_query_1="LOAD DATA LOCAL INPATH '"+filepath_1+"'OVERWRITE INTO TABLE Table1"
+load_query_2="LOAD DATA LOCAL INPATH '"+filepath_2+"'OVERWRITE INTO TABLE Table2"
+hivecontext.sql("DROP TABLE IF EXISTS Table1")
+hivecontext.sql("DROP TABLE IF EXISTS Table2")
+hivecontext.sql("CREATE TABLE IF NOT EXISTS Table1(k1 STRING,v1 STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE")
+hivecontext.sql(load_query_1)
+hivecontext.sql("CREATE TABLE IF NOT EXISTS Table2(k1 STRING,v1 STRING)ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE")
+hivecontext.sql(load_query_2)
+results_1=hivecontext.sql("SELECT * FROM Table1")
+results_1.show()
+results_2=hivecontext.sql("SELECT * FROM Table2")
+results_2.show()
+
+#hivecontext.sql("INSERT INTO TABLE Table1 VALUES ('1234','Alabama'),('3245','California'),('4567','Texas'),('7686','Utah'),('9872','Alaska')")
+
+
+#Equality Join
+results=hivecontext.sql("SELECT Table1.* FROM Table1 JOIN Table2 ON (Table1.v1=Table2.k1)")
+results.show()
+
+#Left Outer Join
+results=hivecontext.sql("SELECT Table1.k1,Table1.v1,Table2.k1,Table2.v1 FROM Table1 LEFT OUTER JOIN Table2 ON (Table1.v1=Table2.k1)") 
+results.show()
+
+#Right Outer Join
+results=hivecontext.sql("SELECT Table1.k1,Table1.v1,Table2.k1,Table2.v1 FROM Table1 RIGHT OUTER JOIN Table2 ON (Table1.v1=Table2.k1)")
+results.show()
+
+#Full Outer Join
+results=hivecontext.sql("SELECT Table1.k1,Table1.v1,Table2.k1,Table2.v1 FROM Table1 FULL OUTER JOIN Table2 ON (Table1.v1=Table2.k1)")
+results.show()
+
+
 
 ##############Scraped code#################################################
 #Test : Use Regex serde to store text file
